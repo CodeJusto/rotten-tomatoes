d44a233b1bcd9b4cf9c4e9925adb4dfca8e70564
Justin Wong <justincwong89@gmail.com>
2016-05-24 22:15:30 +0000

    Added ability to edit new users as admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,19 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.order(:firstname).page(params[:page])
   end
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to movies_path, notice: "Welcome aboard, #{@user.firstname}!"
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
 end
